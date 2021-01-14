bb92be286975920a6359ae67e824a88cc7ed98b2
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-01-14 23:14:32 +0900

    詳細表示ページで画像を表示するため

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,7 +21,7 @@
                       @if($post->image == null)
                         <img src="/storage/no-image.png" class="img-responsive d-block mx-auto" style="max-width: 100%; height: auto; width /***/:auto;">
                       @else
-                        <img src="/storage/{{$post->image}}" class="img-responsive d-block mx-auto" style="max-width: 100%; height: auto; width /***/:auto;">
+                        <img src="{{$post->image}}" class="img-responsive d-block mx-auto" style="max-width: 100%; height: auto; width /***/:auto;">
                       @endif
                     </div>
                     
